c5a993c9a550092a443721ce2eb0c4538a92071c
Suresh Krishnan <suresh.krishnan@amido.com>
2022-07-08 10:27:09 +0100

    Task-4746 : Refactor Swagger annotations to reduce duplication

---
+++ java/src/main/java/com/amido/stacks/core/api/dto/response/ResourceUpdatedResponse.java
@@ -0,0 +1,19 @@
+package com.amido.stacks.core.api.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+/**
+ * @author ArathyKrishna
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResourceUpdatedResponse {
+  @JsonProperty("id")
+  private UUID id = null;
+}
